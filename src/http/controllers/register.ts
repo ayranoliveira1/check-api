68ed804cd99af2bf67cb60b8ba6d9fa4eb041180
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-02-26 15:01:55 -0300

    feat: ensure that the user's password is encrypted

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
+import { hash } from "bcryptjs";
+
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodyshchema = z.object({
     name: z.string(),
@@ -11,13 +13,27 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodyshchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: "User with same email already exists",
+    });
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
-  reply.status(201).send();
+  return reply.status(201).send();
 }
