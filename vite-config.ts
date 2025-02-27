e108d3ac6ca464d3aa41b7f597665bdcfa9f9fc1
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-02-27 12:14:22 -0300

    feat: add im memmory users repository

--- vite-config.ts
+++ vite-config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vitest/config";
-import tsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
+  plugins: [tsconfigPaths()],
 });
