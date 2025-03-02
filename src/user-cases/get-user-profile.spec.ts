e2dd89da12ad5680629436b26035e4facb2bee17
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-02 19:56:50 -0300

    feat: add get user profile test

---
+++ src/user-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-repositry";
+import { hash } from "bcryptjs";
+import { GetUserProfileUseCase } from "./get-user-profile";
+import { ResourceNotFoundError } from "./erros/resource-not-found-error";
+
+let usersRepository: InMemoryUsersRepository;
+let getUserProfileUseCase: GetUserProfileUseCase;
+
+describe("Get User Profile use case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    getUserProfileUseCase = new GetUserProfileUseCase(usersRepository);
+  });
+
+  it("should be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      name: "John Doe",
+      email: "ayranoliveira1@gmail.com",
+      password_hash: await hash("12345678", 6),
+    });
+
+    const { user } = await getUserProfileUseCase.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+    expect(user.name).toEqual("John Doe");
+  });
+
+  it("should not be able to get user profile with wrpong id", async () => {
+    await expect(async () => {
+      await getUserProfileUseCase.execute({
+        userId: "not-exists-id",
+      });
+    }).rejects.instanceOf(ResourceNotFoundError);
+  });
+});
