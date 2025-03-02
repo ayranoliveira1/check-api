ff2ce4028f930db275fbf8da14c68ce82ef64dff
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-02 19:57:37 -0300

    feat: add findById repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../user-repository";
 
 export class PrismaUsersReposity implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    const user = prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findFirst({
       where: {
