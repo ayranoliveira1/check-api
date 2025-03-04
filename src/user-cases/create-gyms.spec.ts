c2a52437205edac1d5e3c449ac68a8da03e46674
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-04 14:11:16 -0300

    feat: add create gyns use case

---
+++ src/user-cases/create-gyms.spec.ts
@@ -0,0 +1,26 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { RegisterUseCase } from "./register";
+import { InMemoryGymsRepository } from "../repositories/in-memory/in-memory-gyms-repository";
+import { GymsUseCase } from "./create-gyms";
+
+let gymsRepository: InMemoryGymsRepository;
+let gymsUseCase: GymsUseCase;
+
+describe("Register use case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    gymsUseCase = new GymsUseCase(gymsRepository);
+  });
+
+  it("should be able to register", async () => {
+    const { gym } = await gymsUseCase.execute({
+      title: "Academia",
+      description: "Academia de musculação",
+      phone: "9293989389",
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    });
+
+    expect(gym.id).toEqual(expect.any(String));
+  });
+});
