2bbf2366e447225ca21ad51933816302c56bb576
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-13 16:12:30 -0300

    chore: add jwt setup

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import fastify from "fastify";
 import { apppRoutes } from "./http/route";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(apppRoutes);
 
