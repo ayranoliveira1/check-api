2abd855da8ded77e6f3859bbed9aeff6b716f0f5
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-13 16:14:26 -0300

    feat: add profile controller

--- src/http/route.ts
+++ src/http/route.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
+import { profile } from "./controllers/profile";
 import { authenticate } from "./controllers/authenticate";
+import { VerifyJwt } from "@/middlewares/verify-jwt";
 
 export async function apppRoutes(app: FastifyInstance) {
   app.post("/user", register);
 
   app.post("/sessions", authenticate);
+
+  // Authenticated
+  app.get("/profile", { onRequest: [VerifyJwt] }, profile);
 }
