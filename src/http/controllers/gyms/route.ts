ef13509572958d4afd99ed2afc94aaf30e77cb84
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-18 10:50:20 -0300

    feat: add gyms routesa

--- src/http/controllers/gyms/route.ts
+++ src/http/controllers/gyms/route.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { VerifyJwt } from "../../../middlewares/verify-jwt";
+import { createGyms } from "./create-gyms";
+import { searchGyms } from "./search-gyms";
+import { nearbyGyms } from "./nearby-gyns";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", VerifyJwt);
+
+  app.get("/gyms/search", searchGyms);
+  app.get("/gyms/nearby", nearbyGyms);
+
+  app.post("/gyms", createGyms);
 }
