4592d0681ceac77fb84e83bfb696f7505fd2dbeb
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-18 18:37:48 -0300

    feat: ensure that the user token can be refreshed

--- src/http/controllers/users/route.ts
+++ src/http/controllers/users/route.ts
@@ -3,11 +3,13 @@ import { register } from "./register";
 import { profile } from "./profile";
 import { authenticate } from "./authenticate";
 import { VerifyJwt } from "../../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
 
   app.post("/sessions", authenticate);
+  app.patch("/token/refresh", refresh);
 
   // Authenticated
   app.get("/profile", { onRequest: [VerifyJwt] }, profile);
