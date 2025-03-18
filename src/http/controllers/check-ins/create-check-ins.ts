f66916cf3a272cbdb7cef815f4431b39242e038f
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-18 11:25:27 -0300

    feat: add create check ins controller

---
+++ src/http/controllers/check-ins/create-check-ins.ts
@@ -0,0 +1,34 @@
+import { makeChekInUseCase } from "@/use-cases/factories/make-check-in-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function createCheckIns(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const createCheckInsParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  });
+
+  const createCheckInsBodySchema = z.object({
+    userLatitude: z.number().refine((value) => Math.abs(value) <= 90),
+    userLongitude: z.number().refine((value) => Math.abs(value) <= 180),
+  });
+
+  const { gymId } = createCheckInsParamsSchema.parse(request.params);
+
+  const { userLatitude, userLongitude } = createCheckInsBodySchema.parse(
+    request.body
+  );
+
+  const checkInsUseCase = makeChekInUseCase();
+
+  await checkInsUseCase.execute({
+    userId: request.user.sub,
+    gymId,
+    userLatitude,
+    userLongitude,
+  });
+
+  reply.status(201).send();
+}
