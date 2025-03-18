8c66550cd088c0af0add5beeba5e3075a0cdef44
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-18 10:49:04 -0300

    feat: add create gyms controller

---
+++ src/http/controllers/gyms/create-gyms.ts
@@ -0,0 +1,28 @@
+import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function createGyms(request: FastifyRequest, reply: FastifyReply) {
+  const createGymsBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  });
+
+  const { title, description, phone, latitude, longitude } =
+    createGymsBodySchema.parse(request.body);
+
+  const createGynsUseCase = makeCreateGymUseCase();
+
+  await createGynsUseCase.execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  });
+
+  reply.code(200).send();
+}
